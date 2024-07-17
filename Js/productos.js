b6ab8b29a4440bf80cdbefc5a275ffdde60cc468
hernanjulio <hernanjuliojaramillo@gmail.com>
2024-07-17 12:45:47 -0300

    first commit

---
+++ Js/productos.js
@@ -0,0 +1,98 @@
+const productos = [
+	{
+		id: 1,
+		nombre: 'Camiseta Aldosivi',
+		precio: 90.000,
+		img: 'camiseta-aldosivi.webp',
+		stock: 20,
+		descripcion: "camiseta aldosivi"
+	},
+	{
+		id: 2,
+		nombre: 'Alternativa Argetina',
+		precio: 119.000,
+		img: 'camiseta-argentina.jpeg',
+		stock: 32,
+		descripcion: "camiseta argentina"
+	},
+	{
+		id: 3,
+		nombre: 'Camiseta Boca',
+		precio: 115.000,
+		img: 'camiseta-boca.jpeg',
+		stock: 30,
+		descripcion: "camiseta boca"
+	},
+	{
+		id: 4,
+		nombre: 'Camiseta Independiente',
+		precio: 145.000,
+		img: 'camiseta-inde.jpeg',
+		stock: 26,
+		descripcion: "camiseta independiente"
+	},
+	{
+		id: 5,
+		nombre: 'Camiseta River',
+		precio: 120.000,
+		img: 'camiseta-river.jpeg',
+		stock: 18,
+		descripcion: "camiseta river"
+	},
+	{
+		id: 6,
+		nombre: 'Camiseta Juventus',
+		precio: 120.000,
+		img: 'camiseta-juve.jpeg',
+		stock: 52,
+		descripcion: "camiseta juve"
+	},
+	{
+		id: 7,
+		nombre: 'Camiseta Racing',
+		precio: 120.000,
+		img: 'camiseta-racing.jpeg',
+		stock: 30,
+		descripcion: "camiseta racing"
+	},
+	{
+		id: 8,
+		nombre: 'Camiseta Campeon del Mundo',
+		precio: 100.000,
+		img: 'camiseta-argentina-negra.webp',
+		stock: 26,
+		descripcion: "camiseta argetina negra"
+	},
+	{
+		id: 9,
+		nombre: 'Camiseta Brasil',
+		precio: 120.000,
+		img: 'camiseta.brasil.jpeg',
+		stock: 18,
+		descripcion: "camiseta brasil"
+	},
+	{
+		id: 10,
+		nombre: 'Camiseta Barcelona',
+		precio: 120.000,
+		img: 'camiseta-barcelona.webp',
+		stock: 52,
+		descripcion: "camiseta barcelona"
+	},
+	{
+		id: 11,
+		nombre: 'Camiseta Barcelona',
+		precio: 120.000,
+		img: 'camiseta-barcelona.webp',
+		stock: 52,
+		descripcion: "camiseta barcelona"
+	},
+	{
+		id: 12,
+		nombre: 'Camiseta Barcelona',
+		precio: 120.000,
+		img: 'camiseta-barcelona.webp',
+		stock: 52,
+		descripcion: "camiseta barcelona"
+	}
+];
